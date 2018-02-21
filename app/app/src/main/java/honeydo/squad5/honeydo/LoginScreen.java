3cb185dbca486c9f85e7784a3f2056d3da5f60e8
Aaron Chambers <aaron.chambers81@gmail.com>
2018-02-21 13:57:56 -0500

    added internet permission

--- app/app/src/main/java/honeydo/squad5/honeydo/LoginScreen.java
+++ app/app/src/main/java/honeydo/squad5/honeydo/LoginScreen.java
@@ -1,20 +1,60 @@
 package honeydo.squad5.honeydo;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONObject;
 
 public class LoginScreen extends AppCompatActivity {
+
+    public JSONObject dummy;
+
+    private EditText email, password;
+    private Button loginBtn, newBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        getJSON();
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        loginBtn = (Button)findViewById(R.id.login);
+        newBtn = (Button)findViewById(R.id.signup);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                attemptLogin();
+            }
+        });
     }
 
-    private boolean attemptLogin(String user, String pass)
+    private void getJSON(){
+        String dummyStr = "{\"user\":\"test\",\"password\":\"admin\"}";
+        try {
+            dummy = new JSONObject(dummyStr);
+        } catch (Throwable t)
+        {
+
+        }
+
+    }
+
+    private void attemptLogin()
     {
-        return true;
+        String emailStr = email.getText().toString();
+        String passStr = password.getText().toString();
+
+        Intent intent = new Intent(this, MainScreen.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+        startActivity(intent);
+        this.finish();
     }
 }
---
+++ app/app/src/main/java/honeydo/squad5/honeydo/api/API.java
@@ -0,0 +1,9 @@
+package honeydo.squad5.honeydo.api;
+
+/**
+ * Created by aaron on 2/21/2018.
+ */
+
+public class API {
+
+}
