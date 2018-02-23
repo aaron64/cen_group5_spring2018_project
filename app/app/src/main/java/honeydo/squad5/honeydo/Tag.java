3527602ce0156b30e9c5b9200f8ed0402c4f5a02
Aaron Chambers <aaron.chambers81@gmail.com>
2018-02-23 00:30:57 -0500

    adding some objects

---
+++ app/app/src/main/java/honeydo/squad5/honeydo/Tag.java
@@ -0,0 +1,17 @@
+package honeydo.squad5.honeydo;
+
+import android.graphics.Color;
+
+/**
+ * Created by aaron on 2/23/2018.
+ */
+
+
+public class Tag {
+    private Color color;
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+}
